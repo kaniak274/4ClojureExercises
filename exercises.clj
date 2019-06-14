5073d8058e87fcc7b28ba33b916317ef117e96a0
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-06-14 16:08:28 +0200

    Create exercises.clj

---
+++ exercises.clj
@@ -0,0 +1,105 @@
+;; Answers for 4clojure.com first 100 questions.
+
+; 1. Nothing but the Truth
+true
+
+; 2. Simple Math
+4
+
+; 3. Intro to Strings
+"HELLO WORLD"
+
+; 4. Intro to Lists
+:a :b :c
+
+; 5. Lists: conj
+'(1 2 3 4)
+
+; 6. Intro to Vectors
+:a :b :c
+
+; 7. Vectors: conj
+'[1 2 3 4]
+
+; 8. Intro to Sets
+#{:a :b :c :d}
+
+; 9. Sets: conj
+1 4 3 2
+
+; 10. Intro to Maps
+20
+
+; 11. Maps: conj
+{:b 2}
+
+; 12. Intro to Sequences
+3
+
+; 13. Sequences: rest
+'(20 30 40)
+
+; 14. Intro to Functions
+8
+
+; 15. Double Down
+(partial * 2)
+
+; 16. Hello World
+(fn [name] (str "Hello, " name "!"))
+
+; 17. Sequences: map
+'(6 7 8)
+
+; 18. Sequences: filter
+'(6 7)
+
+; 19. Local bindings
+7
+
+; 20. Let it Be
+[x 7 y 3 z 1]
+
+; 21. Regular Expressions
+"ABC"
+
+; 22. Intro to Reduce
++
+
+; 23. Simple Recursion
+'(5 4 3 2 1)
+
+; 24. Rearranging Code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
